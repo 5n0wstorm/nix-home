63c27042ff00d4a32653a0f775f0eb007804db12
Dominik Prange <d.prange@service-plus-gmbh.de>
2025-11-29 19:30:14 +0100

    a

--- modules/networking/reverse-proxy.nix
+++ modules/networking/reverse-proxy.nix
@@ -162,7 +162,7 @@ with lib; {
 
       locations."/" = {
         proxyPass = "http://${target}:${toString port}";
-        proxyWebsockets = labels."fleet.reverse-proxy.websockets" == "true";
+        proxyWebsockets = (labels."fleet.reverse-proxy.websockets" or "false") == "true";
         extraConfig = ''
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
