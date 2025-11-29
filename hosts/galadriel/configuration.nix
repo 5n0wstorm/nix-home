a5d4dbfca1bda45df2e7d5790ea3cc95b5489c2d
Dominik Prange <d.prange@service-plus-gmbh.de>
2025-11-29 17:06:29 +0100

    hm

--- hosts/galadriel/configuration.nix
+++ hosts/galadriel/configuration.nix
@@ -170,11 +170,11 @@ in {
   # ============================================================================
 
   # Git configuration with fallback identity
-  programs.git.enable = true
-  programs.git.config = {
-    userName = "$(cat /run/secrets/git_user_name)";
-    userEmail = "$(cat /run/secrets/git_user_email)";
-  };
+  programs.git.enable = true;
+
+  # Git identity from secrets:
+  git config --global user.name "$(cat /run/secrets/git_user_name)"
+  git config --global user.email "$(cat /run/secrets/git_user_email)"
 
   # ============================================================================
   # BOOTLOADER
