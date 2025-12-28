052f0aa6ef1175b1b350e77dc91eb0f5a56df735
Dominik Prange <d.prange@service-plus-gmbh.de>
2025-12-28 18:18:12 +0100

    d

--- modules/apps/nextcloud.nix
+++ modules/apps/nextcloud.nix
@@ -459,7 +459,7 @@ in {
 
       # Install basic apps
       extraApps = {
-        inherit (nextcloudPkg.packages.apps) contacts calendar tasks previewgenerator duplicatefinder;
+        inherit (nextcloudPkg.packages.apps) contacts calendar tasks previewgenerator;
       };
       extraAppsEnable = true;
     };
